2ee13cd6c62664b276152cdbc96aed8ee057c506
«Kristina» <kristinabol1996@gmail.com>
2022-09-04 05:22:17 +0800

    1 задача, печать чисел от M до N

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 5, 6, 7, 8""
+
+void PrintNumbers(int M, int N)
+{
+    if (M > N)
+        return;
+    Console.Write($"{M} ");
+    PrintNumbers(M + 1, N);
+}
+
+PrintNumbers(4, 8);
+Console.WriteLine();
+PrintNumbers(1, 5);
