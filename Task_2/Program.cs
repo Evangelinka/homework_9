6d17491231af4471f88b1acec165652875ca9654
«Kristina» <kristinabol1996@gmail.com>
2022-09-04 05:21:27 +0800

    2 задача, сложение чисел от M до N

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int SumNumbers(int M, int N)
+{
+    if (M > N) return 0;
+    return (M + SumNumbers(M + 1, N));
+}
+
+Console.WriteLine(SumNumbers(4, 8));
+Console.WriteLine(SumNumbers(1, 15));
